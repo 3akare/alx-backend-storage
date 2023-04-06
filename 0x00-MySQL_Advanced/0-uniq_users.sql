ada7c1aa9450ff938b3227a99741165e7da831f6
David Bakare <bakaredavid007@gmail.com>
2023-04-06 02:20:02 +0100

    corrected table schema

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create a table users if it doesn't exist
 
 CREATE TABLE IF NOT EXISTS `users`(
-    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
