99b6b3769874021865fdcc5e31f2fe8629235424
3akare <bakaredavid007@gmail.com>
2023-04-08 18:14:27 +0000

    removed limit

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- Ranks Country Origins of Bands Ordered by the number of (non-unique) fans
-
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER by nb_fans DESC
-LIMIT 10;
