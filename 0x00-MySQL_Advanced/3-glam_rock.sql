14209b8d45cdfae17adf496049e574262061b350
3akare <bakaredavid007@gmail.com>
2023-04-08 17:54:25 +0000

    Query must work in every database

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- List all the bands with Glam Rock as their main style
--- Because Glam Rock is the best
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM holberton.metal_bands
-WHERE FIND_IN_SET("Glam Rock", IFNULL(style, "")) > 0
+
+SELECT band_name, (COALESCE(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam Rock%'
 ORDER BY lifespan DESC;
