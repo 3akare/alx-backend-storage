0a40c01ec7e12681a3c288724ca499ee8bd00268
3akare <bakaredavid007@gmail.com>
2023-04-08 17:51:53 +0000

    changed getYear() to '2020'

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- List all the bands with Glam Rock as their main style
-
-SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+-- Because Glam Rock is the best
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 FROM holberton.metal_bands
 WHERE FIND_IN_SET("Glam Rock", IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
