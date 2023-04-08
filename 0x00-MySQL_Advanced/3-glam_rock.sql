57a66c3de041dcd476b5dff26dada36a348ab142
3akare <bakaredavid007@gmail.com>
2023-04-08 17:48:23 +0000

    code refactor

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- List all the bands with Glam Rock as their main style
 
-SELECT band_name, (COALESCE(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 FROM holberton.metal_bands
-WHERE style LIKE '%Glam Rock%'
+WHERE FIND_IN_SET("Glam Rock", style)
 ORDER BY lifespan DESC;
