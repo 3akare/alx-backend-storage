2cb0aa6734011a473fc1d05ce76598048527a337
3akare <bakaredavid007@gmail.com>
2023-04-09 13:33:28 +0000

    adjusted year

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- List all the bands with Glam Rock as their main style
-SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE()) - 1) - formed) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
