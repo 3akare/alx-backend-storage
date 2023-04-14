a5f79d022907c8e93fb00b5c5628e92498720da2
David Bakare <bakaredavid007@gmail.com>
2023-04-14 23:15:40 +0100

    insert school

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+insert a new document into a collection
+'''
+
+import pymongo
+client = pymongo.MongoClient()
+
+def insert_school(mongo_collection, **kwargs):
+    obj = {}
+    for k, v in kwargs.items():
+        diction[k] = v
+    mongo_collection.insert_one(obj)
+    return mongo_collection_collection.find(obj).id
